63dcf5b662a3dc5a34bdf5d9224d70fd4005b98d
Alexander (Sasha) Pastukhov <pastukhov.alexander@gmail.com>
2023-02-24 21:43:38 +0100

    Flappy Bird initial

---
+++ solutions/08-flappy-bird/code01.py
@@ -0,0 +1,28 @@
+"""
+Flappy Bird game.
+"""
+
+import json
+
+from psychopy import event, visual
+
+# getting settings
+with open('settings.json') as json_file:
+    settings = json.load(json_file)
+
+win = visual.Window(size=settings['Window']['Size'])
+
+# visuals
+bird = visual.ImageStim(win, settings["Bird"]["Image"])
+
+# main loop
+show_must_go_on = True
+while show_must_go_on:
+    # visuals
+    bird.draw()
+    win.flip()
+
+    # inputs check
+    show_must_go_on = len(event.getKeys(keyList=["escape"])) == 0
+
+win.close()
